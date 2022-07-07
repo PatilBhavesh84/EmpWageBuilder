a770333e64d9c699dfadaadb7aacddd8248e7ec4
PatilBhavesh84 <bhaveshpatil8448@gmail.com>
2022-07-07 14:24:46 +0530

    Added:Month wage total

--- src/empWageBuilder/EmpWageBulider.java
+++ src/empWageBuilder/EmpWageBulider.java
@@ -5,35 +5,35 @@ public class EmpWageBulider {
 	public static final int PART_TIME=1;
 	public static final int FULL_TIME=2;
 	public static final int PER_HOUR=20;
-
+	public static final int WORKING_DAYS=20;
 	public static void main(String[] args) {
 
-		int empHr=0;
-		int empWage=0; 
-
-		int empCheck=(int)Math.floor(Math.random()*10)%3;
-
-		switch(empCheck)
+		int totalWage=0;
+		int empHr=0; 
+		for(int day=1;day<=WORKING_DAYS;day++)
 		{
-		case PART_TIME:
-			empHr=4;
-			System.out.println("part time");
-			break;
 
-		case FULL_TIME:
-			empHr=8;
-			System.out.println("full time");
-			break;
+			int empCheck=(int)Math.floor(Math.random()*10)%3;
 
-		default:
-			empHr=0;
-			System.out.println("absent");
+			switch(empCheck)
+			{
+			case PART_TIME:
+				empHr=4;
 
+				break;
 
+			case FULL_TIME:
+				empHr=8;
 
+				break;
 
-			empWage=empHr*PER_HOUR;
-			System.out.println("Employee wage-" +empWage);
+			default:
+			}
+
+			int wage=empHr*PER_HOUR;
+			System.out.println("On day-"+day+"-wage is-"+wage);
+			totalWage += wage;
+			System.out.println("Total wage for a month is-" +totalWage);
 		}
 	}
 }

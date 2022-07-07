db95cfcfde788076d8ed9c08cd0c94c36ea70f54
PatilBhavesh84 <bhaveshpatil8448@gmail.com>
2022-07-07 12:49:18 +0530

    Added:Daily Wage

--- src/empWageBuilder/EmpWageBulider.java
+++ src/empWageBuilder/EmpWageBulider.java
@@ -4,12 +4,18 @@ public class EmpWageBulider {
 
 	public static void main(String[] args) {
 		int PRESENT=1;
+		int PER_HOUR=20;
+
+		int empHr=0;
+		int empWage=0; 
 		double empCheck=Math.floor(Math.random()*10)%2;
 		if(empCheck==PRESENT)
-			System.out.println("Employee is present");
+			empHr=8;
 		else
-			System.out.println("Employee is absent");
+			empHr=0;
 
+		empWage=empHr*PER_HOUR;
+		System.out.println("Employee wage-" +empWage);
 	}
 
 }
